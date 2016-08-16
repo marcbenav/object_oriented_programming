d0fa046c2b583b6c3e4e365c241c93e313cf308c
marcbenav <m.benav86@gmail.com>
2016-08-16 18:27:43 -0400

    Created two instances of cat

--- cat.rb
+++ cat.rb
@@ -3,6 +3,8 @@ class Cat
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
-    
+
   end
 end
+Rodney = Cat.new("Rodney", "Mariner's Catch", 21)
+Herschel = Cat.new("Herschel", "Chicken Dinner", 0)
