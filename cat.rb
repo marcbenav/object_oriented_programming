5ce74481634308693b574b8c1b06a13be94fa4bb
marcbenav <m.benav86@gmail.com>
2016-08-16 21:17:49 -0400

    Created method to return all values

--- cat.rb
+++ cat.rb
@@ -14,7 +14,15 @@ def eats_at
     return "#{@meal_time} AM"
   end
 end
+def meow
+  return "Greetings, my name is #{@name}, and I love eating #{@preferred_food} at
+  #{eats_at}."
+end
+def meow
+  return "Hello, my name is #{@name}, and I love eating #{@preferred_food} at #{eats_at}."
+end
+  Rodney = Cat.new("Rodney", "Mariner's Catch", 21)
+  Herschel = Cat.new("Herschel", "Chicken Dinner", 0)
 
-Rodney = Cat.new("Rodney", "Mariner's Catch", 21)
-Herschel = Cat.new("Herschel", "Chicken Dinner", 0)
+puts Rodney.meow
 end
