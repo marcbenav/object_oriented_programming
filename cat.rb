2c19147c6bb9bd4a89dcdd56eb0b58ad9eb8f098
marcbenav <m.benav86@gmail.com>
2016-08-16 20:58:45 -0400

    Created method to return hour at which the cats eat

--- cat.rb
+++ cat.rb
@@ -3,8 +3,19 @@ class Cat
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
-
-  end
 end
+
 Rodney = Cat.new("Rodney", "Mariner's Catch", 21)
 Herschel = Cat.new("Herschel", "Chicken Dinner", 0)
+
+def eats_at
+  if @meal_time > 12
+    return "#{@meal_time - 12} PM"
+  elsif @meal_time == 0
+    return "Midnight"
+  else
+    return "#{@meal_time} AM"
+  end
+end
+
+end
